db6206a8b66d94c6b0c49a8721a315907bfaabcc
Jessica Garrido <jessica.garrido.di@gmail.com>
2023-03-20 11:30:02 +0100

    rafraîchissement des couleurs terminé

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const refreshBtn = document.querySelector(".refresh-btn");
 const maxPaletteBoxes = 32;
 
 const generatePalette = () => {
+    container.innerHTML = "";
+
     for (let i = 0; i < maxPaletteBoxes; i++) {
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
     randomHex = `#${randomHex.padStart(6, "0")}`;
@@ -13,9 +15,8 @@ const generatePalette = () => {
     color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
                        <span class="hex-value">${randomHex}</span>`
     container.appendChild(color);
-    }
-
-    
+    } 
 }
+generatePalette();
 
 refreshBtn.addEventListener("click", generatePalette);
